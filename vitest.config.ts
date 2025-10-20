d2cf62ca081fd2fd5b953f10c2c71351fa2e52ed
SKDesing <votre_email_github>
2025-10-21 01:03:26 +0200

    chore(tests): add test deps and fix configs

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
     globals: true,
-    environment: 'jsdom',
+    environment: "jsdom",
     setupFiles: [],
     coverage: {
-      provider: 'c8'
-    }
-  }
-})
+      provider: "v8",
+    },
+  },
+});
